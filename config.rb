3a81069a141e5f9f9f05b2566f603c0d7ed10a68
Fernando Moreira <nandomoreira.me@gmail.com>
2015-09-10 16:41:16 -0300

    assets

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "./_source/assets/css/"
+images_dir = "./_source/assets/images/"
+javascripts_dir = "./_source/assets/js/"
+sass_dir = "./_source/_sass/"
+
+output_style = :expanded #:expanded or :nested or :compact or :compressed
+color_output = true
+relative_assets = false
+line_comments = false
+# environment = :development
